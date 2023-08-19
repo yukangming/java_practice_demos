c8baca5060bce61ae73aec6ddd226b2ef997d9c9
yukangming <1540622095@qq.com>
2023-08-19 21:32:39 +0800

    mybatis练习第一个环境框架

---
+++ mybatisDemo/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.apache.ibatis.io.Resources;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
